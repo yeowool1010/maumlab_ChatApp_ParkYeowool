3b8060f00542718f430c408a9d894dcd9851f17b
yeowool1010 <qyoong3579@gmail.com>
2023-01-24 04:24:08 +0900

    💄 [ui] 채팅방 목록 화면구현 완료 #12

--- chatApp/renderer/components/ChatRoom.tsx
+++ chatApp/renderer/components/ChatRoom.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import Link from "next/link";
+import ChatIcon from "../public/ChatIcon";
+import GroupChatIcon from "../public/GroupChatIcon";
 
 function ChatRoom() {
   return (
-    <div className="flex w-full h-screen bg-mainBg">
-      <div className="flex flex-col">
-        <p>⚡ 채팅방 리스트 ⚡ -</p>
-        <div className="w-full  bg-mainBg">
-          <div className="flex items-center bg-white w-10/12 h-10 pl-2">
-            <div>
-              <p className="">채팅방</p>
-            </div>
+    <div className="flex justify-center items-center h-screen bg-mainBg">
+      <div className="flex flex-col w-full h-screen px-3">
+        <div className="font-bold text-xl mb-5 text-left">채팅</div>
+        <div className="w-full bg-mainBg">
+          <div className="flex justify-between items-center bg-white cursor-pointer	 w-full h-10 px-4 mb-2">
+            <p className="">채팅방1</p>
+            <ChatIcon />
+          </div>
+          <div className="flex justify-between items-center bg-white cursor-pointer	 w-full h-10 px-4 mb-2">
+            <p className="">그룹채팅방</p>
+            <GroupChatIcon />
+          </div>
+          <div className="flex justify-between items-center bg-white cursor-pointer	 w-full h-10 px-4 mb-2">
+            <p className="">채팅방2</p>
+            <ChatIcon />
           </div>
         </div>
       </div>
