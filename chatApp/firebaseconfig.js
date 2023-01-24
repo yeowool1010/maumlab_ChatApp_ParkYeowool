44bebe52544f94da9e64be8a8edbd271b33aba4a
yeowool1010 <qyoong3579@gmail.com>
2023-01-25 00:48:25 +0900

    📝 [docs] firebaseConfig 수정

--- chatApp/firebaseconfig.js
+++ chatApp/firebaseconfig.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -20,4 +21,6 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
-export { auth };
+const db = getFirestore();
+
+export { auth, db };
