de7bd4b5f75c2c65076484a42e744d5d92b60831
yeowool1010 <qyoong3579@gmail.com>
2023-01-24 23:16:15 +0900

    📝 [docs] firebase 설치 config.js설

---
+++ chatApp/firebaseconfig.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCZDbNdY3t9tN9MnniiDAmibw9oF7Q0OwU",
+  authDomain: "maumlab-chatapp-parkyeowool.firebaseapp.com",
+  projectId: "maumlab-chatapp-parkyeowool",
+  storageBucket: "maumlab-chatapp-parkyeowool.appspot.com",
+  messagingSenderId: "158470836955",
+  appId: "1:158470836955:web:36ac62e873f48ecc4932fe",
+  measurementId: "G-232QT20K4T",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
+export { auth };
