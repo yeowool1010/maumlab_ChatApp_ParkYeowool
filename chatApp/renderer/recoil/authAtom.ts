c1b4d6e3e1136ca1827d3c1aca5540acee23b318
yeowool1010 <qyoong3579@gmail.com>
2023-01-25 00:49:16 +0900

    ✨ [feat] 로그인 전역 상태 관리

--- chatApp/renderer/recoil/authAtom.ts
+++ chatApp/renderer/recoil/authAtom.ts
@@ -1,19 +1,19 @@
 import { atom } from "recoil";
 
-const localStorage = typeof window !== "undefined" ? window.localStorage : null;
+// const localStorage = typeof window !== "undefined" ? window.localStorage : null;
 const isLoginState = atom({
   key: "isLogin",
-  default: localStorage?.getItem("token") ? true : false,
+  default: false,
 });
 
-const accessTokenState = atom({
-  key: "token",
-  default: localStorage?.getItem("token") || "",
-});
+// const accessTokenState = atom({
+//   key: "token",
+//   default: localStorage?.getItem("token") || "",
+// });
 
-const userNameState = atom({
-  key: "userName",
-  default: localStorage?.getItem("memberId") || "",
-});
+// const userNameState = atom({
+//   key: "userName",
+//   default: localStorage?.getItem("memberId") || "",
+// });
 
-export { isLoginState, accessTokenState, userNameState };
+export { isLoginState };
